369a38f9b0fa1c247606ef591fa10113ed95c2e4
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-01-24 23:54:32 -0500

    Update font.ts

--- src/font.ts
+++ src/font.ts
@@ -1,36 +1,6 @@
-import type { ITextStyle } from 'pixi.js';
+import type { IBitmapTextStyle } from 'pixi.js';
 
-export const fontDialogue: Partial<ITextStyle> = {
-	fontFamily: 'font',
-	fontSize: 8,
-	padding: 16,
-	fill: 0xffffff,
-	align: 'left',
-	lineHeight: 8 * 1.25,
-	letterSpacing: 0,
-	lineJoin: 'round',
-};
-export const fontPrompt: Partial<ITextStyle> = {
-	fontFamily: 'font',
-	fontSize: 8,
-	padding: 16,
-	fill: 0xffffff,
+export const fontInput: Partial<IBitmapTextStyle> = {
+	fontName: 'bmfont',
 	align: 'center',
-	lineHeight: 8 * 1.25,
-	letterSpacing: 0,
-	stroke: 0,
-	strokeThickness: 2,
-	lineJoin: 'round',
-};
-export const fontIngame: Partial<ITextStyle> = {
-	fontFamily: 'font',
-	fontSize: 8,
-	padding: 16,
-	fill: 0xffffff,
-	align: 'center',
-	lineHeight: 8 * 1.25,
-	letterSpacing: 0,
-	stroke: 0,
-	strokeThickness: 2,
-	lineJoin: 'round',
 };
