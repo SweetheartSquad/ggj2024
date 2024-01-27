489166a77ac00255c7942e62914849aa4a79df2d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2024-01-26 23:03:09 -0500

    replace smart apostrophe

--- src/lines.ts
+++ src/lines.ts
@@ -1,3 +1,3 @@
 export const lines = [
 	`Tis but thy name that is my enemy; Thou art thyself, though not a Montague. What’s Montague? It is nor heel, nor arch, Nor ball, nor toe, nor any other part Belonging to a foot. O, be some other name! What’s in a name? That which we call a foot By any other name would smell as sweet; So Tomeo would, were he not Tomeo call’d, Retain that dear perfection which he owes Without that title. Tomeo, doff thy name, And for that name which is no part of thee Take all my toes.`,
-];
+].map((i) => i.replace(/’/, "'"));
